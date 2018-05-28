f10a5c75fd28b4c8ea82e9b6ebdb6a83efc532f8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-28 15:31:45 +0600

    Library Management using asp.NET core 2.0

--- LibraryManagement/Data/Interfaces/IAuthorRepository.cs
+++ LibraryManagement/Data/Interfaces/IAuthorRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagement.Data.Model;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +9,8 @@ namespace LibraryManagement.Data.Interfaces
 {
     public interface IAuthorRepository
     {
+        IEnumerable<Author> GetAllWithBooks();
 
+        Author GetWithBooks();
     }
 }
