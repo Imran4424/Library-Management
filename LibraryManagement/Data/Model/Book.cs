284dbba4210d71d74a6793c4b85647e3a469e871
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-28 10:19:00 +0600

    Library Management using asp.NET core 2.0

---
+++ LibraryManagement/Data/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Model
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public virtual Author author { get; set; }
+
+        public int  authorId { get; set; }
+
+        public virtual Customer Borrower { get; set; }
+    }
+}
